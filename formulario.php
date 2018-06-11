bf0abebeb93356f01bfde0aa0d8ed66a993c831e
JosueDai <37085942+JosueDai@users.noreply.github.com>
2018-06-11 16:11:05 -0500

    Password

--- formulario.php
+++ formulario.php
@@ -1,4 +1,4 @@
-<form action ="procesar.php">
+<form action ="procesar.php" method="POST">
 <br>
 <label for=""> Escribe un numero</label>
 <input type="text" name="n1">
@@ -6,5 +6,15 @@
 <label for="">Escribe otro numero</label>
 <input type="text" name="n2">
 <br>
+<label for=""> password</label>
+<input type="password" name="pass">
 <button>Enviar</button>
 </form>
+
+<?php
+    if(isset($_POST['suma'])){
+
+      $suma = $_POST['suma'];
+       echo "<h1>La suma es $suma </h1>";
+     }
+?>
--- procesar.php
+++ procesar.php
@@ -1,12 +1,16 @@
   <?php
   
-  if(isset($_GET['n1']) && isset($_GET['n2'])){
-      $num1= $_GET['n1'];
-      $num2= $_GET['n2'];
+  if(isset($_POST['n1']) && isset($_POST['n2'])){
+      $num1= $_POST['n1'];
+      $num2= $_POST['n2'];
 
       $suma = $num1 + $num1;
 
       echo "La suma es: $suma";
+      $sql = 'select * from usuario where password=' .$_POST['pass'];
+      echo 'SQL que se ejecutara es ' .$sql;
+
+    //   header("Location: formulario.php?suma=$suma");
   }else 
    die ("No se puede procesar la informacion.");
 
